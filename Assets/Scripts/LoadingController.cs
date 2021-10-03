0a0ffddfefcd9926e1177005180ce5cc36f7c161
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-10-03 17:04:15 +0700

    first commit

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+{
+    [SerializeField] private Button _localButton, _cloudButton;
+    private void Start()
+    {
+        _localButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false);
+            UserDataManager.LoadFromLocal();
+            SceneManager.LoadScene(1);
+        });
+
+        _cloudButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false);
+            StartCoroutine(UserDataManager.LoadFromCloud(() => SceneManager.LoadScene(1)));
+        });
+    }
+
+    // Mendisable button agar tidak bisa ditekan
+    private void SetButtonInteractable(bool interactable)
+    {
+        _localButton.interactable = interactable;
+        _cloudButton.interactable = interactable;
+    }
+}
